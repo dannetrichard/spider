5984c28319e81ed962454fd01fffdaf5ef2d5bb4
dannetrichard <jinjun0927@126.com>
2017-07-29 07:45:32 +0800

    test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,20 @@
 <?php
 namespace Dannetrichard\Spider\Tests;
+
 use Orchestra\Testbench\TestCase as TestBenchTestCase;
+
+use Dannetrichard\Spider\Spider;
+
 class TestCase extends TestBenchTestCase
 {
     protected function getPackageProviders($app)
     {
         return ['Dannetrichard\Spider\SpiderServiceProvider'];
+    }
+    
+    public function testMultiplyReturnsCorrectValue()
+    {
+        $this->assertSame(Spider::multiply(4, 4), 16);
+        $this->assertSame(Spider::multiply(2, 9), 18);
     }
 }
