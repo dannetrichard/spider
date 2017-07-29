ca998cf67ee910c42d52f2f805155410e465fc7e
dannetrichard <jinjun0927@126.com>
2017-07-29 18:29:58 +0800

    migrations

--- src/SpiderServiceProvider.php
+++ src/SpiderServiceProvider.php
@@ -13,6 +13,9 @@ class SpiderServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        
+        $this->loadMigrationsFrom(__DIR__.'/migrations');
+        
     }
 
     /**
